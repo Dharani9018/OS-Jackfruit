024b934a57ef4dda0813cb5199dd20986861ad42
Dharani9018 <anu293575@gmail.com>
2026-04-05 11:37:49 +0530

    Organize repository

---
+++ boilerplate/engine.h
@@ -0,0 +1,115 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#define _GNU_SOURCE
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <pthread.h>
+#include <sched.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mount.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "monitor_ioctl.h"
+
+#define STACK_SIZE          (1024 * 1024)
+#define CONTAINER_ID_LEN    32
+#define CONTROL_PATH        "/tmp/mini_runtime.sock"
+#define LOG_DIR             "logs"
+#define CONTROL_MESSAGE_LEN 256
+#define CHILD_COMMAND_LEN   256
+#define LOG_CHUNK_SIZE      4096
+#define LOG_BUFFER_CAPACITY 16
+#define DEFAULT_SOFT_LIMIT  (40UL << 20)
+#define DEFAULT_HARD_LIMIT  (64UL << 20)
+
+typedef enum {
+    CMD_SUPERVISOR = 0,
+    CMD_START, CMD_RUN, CMD_PS, CMD_LOGS, CMD_STOP
+} command_kind_t;
+
+typedef enum {
+    CONTAINER_STARTING = 0,
+    CONTAINER_RUNNING,
+    CONTAINER_STOPPED,
+    CONTAINER_KILLED,
+    CONTAINER_EXITED
+} container_state_t;
+
+typedef struct container_record {
+    char id[CONTAINER_ID_LEN];
+    pid_t host_pid;
+    time_t started_at;
+    container_state_t state;
+    unsigned long soft_limit_bytes;
+    unsigned long hard_limit_bytes;
+    int exit_code;
+    int exit_signal;
+    int stop_requested;
+    char log_path[PATH_MAX];
+    struct container_record *next;
+} container_record_t;
+
+typedef struct {
+    char container_id[CONTAINER_ID_LEN];
+    size_t length;
+    char data[LOG_CHUNK_SIZE];
+} log_item_t;
+
+typedef struct {
+    log_item_t items[LOG_BUFFER_CAPACITY];
+    size_t head, tail, count;
+    int shutting_down;
+    pthread_mutex_t mutex;
+    pthread_cond_t not_empty;
+    pthread_cond_t not_full;
+} bounded_buffer_t;
+
+typedef struct {
+    command_kind_t kind;
+    char container_id[CONTAINER_ID_LEN];
+    char rootfs[PATH_MAX];
+    char command[CHILD_COMMAND_LEN];
+    unsigned long soft_limit_bytes;
+    unsigned long hard_limit_bytes;
+    int nice_value;
+} control_request_t;
+
+typedef struct {
+    int status;
+    char message[CONTROL_MESSAGE_LEN];
+} control_response_t;
+
+typedef struct {
+    char id[CONTAINER_ID_LEN];
+    char rootfs[PATH_MAX];
+    char command[CHILD_COMMAND_LEN];
+    int nice_value;
+    int log_write_fd;
+} child_config_t;
+
+typedef struct {
+    int server_fd;
+    int monitor_fd;
+    int should_stop;
+    pthread_t logger_thread;
+    bounded_buffer_t log_buffer;
+    pthread_mutex_t metadata_lock;
+    container_record_t *containers;
+} supervisor_ctx_t;
+
+const char *state_to_string(container_state_t state);
+void usage(const char *prog);
+
+#endif
